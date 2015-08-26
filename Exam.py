76c9281591a193469cb31c16b14a2d86142d9695
renatolrr <renatolrr@gmail.com>
2015-08-26 08:40:11 +0200

    punto 3

--- Exam.py
+++ Exam.py
@@ -1,5 +1,106 @@
 #!/usr/bin/env python
 
-f = open("dna1.fasta", "r")
-file = f.read()
-print(file.count('>'))
+fa="dna.example.fasta"
+
+#How many records are in the file? 
+def contar(fa): 
+	f= open(fa, "r")
+	file = f.read()
+	return file.count('>')
+	
+#What are the lengths of the sequences in the file?
+#What is the longest sequence and what is the shortest sequence? 
+def longuitud(fa):
+	f= open(fa, "r")
+	file = f.readlines()
+	#print file
+	sequences = []
+	seq = ""
+	for f in file:
+		if not f.startswith('>'):
+			f = f.replace(" ", "")
+			f = f.replace("\n", "")
+			seq = seq + f
+		else:
+			sequences.append(seq)
+			seq = ""
+
+	# Add the last seq
+	sequences.append(seq)
+
+	sequences = sequences[1:]
+
+	lengths = [len(i) for i in sequences]
+	#print lengths
+	print "Maximo y minimo:",max(lengths),' ',min(lengths)
+
+#What is the length of the longest ORF in the file?
+def OpenReadingFrame(fa):
+	f = open(fa, "r")
+	file = f.readlines()
+	
+	sequences = []
+	seq = ""
+	for f in file:
+		if not f.startswith('>'):
+			f = f.replace(" ", "")
+			f = f.replace("\n", "")
+			seq = seq + f
+		else:
+			sequences.append(seq)
+			seq = ""
+
+	# Add the last seq
+	sequences.append(seq)
+
+	sequences = sequences[1:]
+
+	# Find orf 2
+	def find_orf_2(sequence):
+		# Find all ATG indexs
+		start_position = 1
+		start_indexs = []
+		stop_indexs = []
+		for i in range(1, len(sequence), 3):
+			if sequence[i:i+3] == "ATG":
+				start_indexs.append(i)
+
+		# Find all stop codon indexs
+		for i in range(1, len(sequence), 3):
+			stops =["TAA", "TGA", "TAG"]
+			if sequence[i:i+3] in stops:
+				stop_indexs.append(i)
+
+		orf = []
+		mark = 0
+		for i in range(0,len(start_indexs)):
+			for j in range(0, len(stop_indexs)):
+				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
+					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
+					mark = stop_indexs[j]+3
+					break
+		return orf
+
+
+	#  [len(i) for i in sequences]
+
+	n = 1
+	lengths = []
+	for i in sequences:
+		orfs = find_orf_2(i)
+		for j in orfs:
+			lengths.append(len(j))
+
+	# orf_lengths = [len(i) for i in orf_2 if i]
+	print 'El maximo Orf es de ',max(lengths)
+	
+#Punto 1	
+HowMany = contar(fa) 
+print ("Hay:" + str(HowMany))
+
+#Punto 2
+longuitud(fa)
+
+#Punto 3
+OpenReadingFrame(fa)
+
