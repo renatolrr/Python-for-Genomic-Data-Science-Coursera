a781c0c0616fcedb15ae184477bccfb7421f619b
renatolrr <renatolrr@gmail.com>
2015-08-26 20:07:52 +0200

    Empaquetando

--- Exam.py
+++ Exam.py
@@ -2,14 +2,7 @@
 
 fa="dna.example.fasta"
 
-#How many records are in the file? 
-def contar(fa): 
-	f= open(fa, "r")
-	file = f.read()
-	return file.count('>')
-	
-#What are the lengths of the sequences in the file?
-#What is the longest sequence and what is the shortest sequence? 
+#Funcion sequencia
 def sequ(fa):
 	f= open(fa, "r")
 	file = f.readlines()
@@ -31,6 +24,48 @@ def sequ(fa):
 	sequences = sequences[1:]
 	
 	return sequences
+	
+# Find all ATG indexs	
+#def atg_index((sequence,n):
+		
+	
+				
+def find_orf(sequence,n):
+		start_position = n-1
+		start_indexs = []
+		stop_indexs = []
+		for i in range(n-1, len(sequence), 3):
+			if sequence[i:i+3] == "ATG":
+				start_indexs.append(i)
+		
+
+		# Find all stop codon indexs
+		for i in range(n-1, len(sequence), 3):
+			stops =["TAA", "TGA", "TAG"]
+			if sequence[i:i+3] in stops:
+				stop_indexs.append(i)
+		orf = []
+		mark = 0
+		for i in range(0,len(start_indexs)):
+			for j in range(0, len(stop_indexs)):
+				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
+					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
+					mark = stop_indexs[j]+3
+					break
+		return orf
+
+	
+#############################################################
+
+#How many records are in the file? 
+def contar(fa): 
+	f= open(fa, "r")
+	file = f.read()
+	return file.count('>')	
+	
+#What are the lengths of the sequences in the file?
+#What is the longest sequence and what is the shortest sequence? 
+
 
 def longuitud(fa):
 	sequences=sequ(fa)
@@ -43,39 +78,11 @@ def longuitud(fa):
 def OpenReadingFrame(fa):
 	sequences=sequ(fa)
 
-	# Find orf 2
-	def find_orf_2(sequence):
-		# Find all ATG indexs
-		start_position = 1
-		start_indexs = []
-		stop_indexs = []
-		for i in range(1, len(sequence), 3):
-			if sequence[i:i+3] == "ATG":
-				start_indexs.append(i)
-
-		# Find all stop codon indexs
-		for i in range(1, len(sequence), 3):
-			stops =["TAA", "TGA", "TAG"]
-			if sequence[i:i+3] in stops:
-				stop_indexs.append(i)
-
-		orf = []
-		mark = 0
-		for i in range(0,len(start_indexs)):
-			for j in range(0, len(stop_indexs)):
-				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
-					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
-					mark = stop_indexs[j]+3
-					break
-		return orf
-
-
 	#  [len(i) for i in sequences]
-
 	n = 1
 	lengths = []
 	for i in sequences:
-		orfs = find_orf_2(i)
+		orfs = find_orf(i,2)
 		for j in orfs:
 			lengths.append(len(j))
 
@@ -131,96 +138,11 @@ def Orf3(fa):
 def punto6(fa):
 	sequences=sequ(fa)
 
-	# Find orf 1
-	def find_orf_1(sequence):
-		# Find all ATG indexs
-		start_position = 0
-		start_indexs = []
-		stop_indexs = []
-		for i in range(0, len(sequence), 3):
-			if sequence[i:i+3] == "ATG":
-				start_indexs.append(i)
-
-		# Find all stop codon indexs
-		for i in range(0, len(sequence), 3):
-			stops =["TAA", "TGA", "TAG"]
-			if sequence[i:i+3] in stops:
-				stop_indexs.append(i)
-
-		orf = []
-		mark = 0
-		start_position = {}
-		for i in range(0,len(start_indexs)):
-			for j in range(0, len(stop_indexs)):
-				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
-					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
-					start_position[len(sequence[start_indexs[i]:stop_indexs[j]+3])] = start_indexs[i]
-					mark = stop_indexs[j]+3
-					break
-		return orf
-
-	# Find orf 2
-	def find_orf_2(sequence):
-		# Find all ATG indexs
-		start_position = 1
-		start_indexs = []
-		stop_indexs = []
-		for i in range(1, len(sequence), 3):
-			if sequence[i:i+3] == "ATG":
-				start_indexs.append(i)
-
-		# Find all stop codon indexs
-		for i in range(1, len(sequence), 3):
-			stops =["TAA", "TGA", "TAG"]
-			if sequence[i:i+3] in stops:
-				stop_indexs.append(i)
-
-		orf = []
-		mark = 0
-		start_position = {}
-		for i in range(0,len(start_indexs)):
-			for j in range(0, len(stop_indexs)):
-				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
-					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
-					start_position[len(sequence[start_indexs[i]:stop_indexs[j]+3])] = start_indexs[i]
-					mark = stop_indexs[j]+3
-					break
-		return orf
-
-	# Find orf 3
-	def find_orf_3(sequence):
-		# Find all ATG indexs
-		start_position = 2
-		start_indexs = []
-		stop_indexs = []
-		for i in range(2, len(sequence), 3):
-			if sequence[i:i+3] == "ATG":
-				start_indexs.append(i)
-
-		# Find all stop codon indexs
-		for i in range(2, len(sequence), 3):
-			stops =["TAA", "TGA", "TAG"]
-			if sequence[i:i+3] in stops:
-				stop_indexs.append(i)
-
-		orf = []
-		mark = 0
-		start_position = {}
-		for i in range(0,len(start_indexs)):
-			for j in range(0, len(stop_indexs)):
-				if start_indexs[i] < stop_indexs[j] and start_indexs[i] > mark:
-					orf.append(sequence[start_indexs[i]:stop_indexs[j]+3])
-					start_position[len(sequence[start_indexs[i]:stop_indexs[j]+3])] = start_indexs[i]
-					mark = stop_indexs[j]+3
-					break
-		return orf
-
-
 	n = 1
 	lengths = []
 	for i in sequences:
 		# print("["+str(n)+"]")
-		orfs = find_orf_1(i) + find_orf_2(i) + find_orf_3(i)
+		orfs = find_orf(i,1) + find_orf(i,2) + find_orf(i,3)
 		for j in orfs:
 			lengths.append(len(j))
 		n += 1
