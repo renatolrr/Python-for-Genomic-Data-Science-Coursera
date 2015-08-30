6224427ffb39bcf1dfe81bfdff3ff95461466e00
renatolrr <renatolrr@gmail.com>
2015-08-30 20:02:16 +0200

    Añadiendo contenido a Exam6_10.py

--- Exam6_10.py
+++ Exam6_10.py
@@ -79,12 +79,89 @@ def punto6(fa):
 	print(max(lengths))
 
 
+#What is the length of the longest ORF that appears in the sequence with the identifier gi|142022655|gb|EQ086233.1|97?
+
+# Find the sequence with the identifier num
+def find_idenfitier(num):
+	f = open(fa, "r")
+	file = f.readlines()
+	seq = ""
+	identifier = 0
+	for i in range(0, len(file)):
+		if num in file[i]:
+			identifier = i
+
+	for f in file[identifier+1:]:
+		if not f.startswith('>'):
+			f = f.replace(" ", "")
+			f = f.replace("\n", "")
+			seq = seq + f
+		else:
+			break
+			
+	lengths = []
+	orfs = find_orf(seq,1) + find_orf(seq,2) + find_orf(seq,3)
+	for j in orfs:
+		lengths.append(len(j))
+
+	print(max(lengths))
+	
+	
+#Find the most frequently occurring repeat of length 6 in all sequences. How many times does it occur in all?
+def find_length(num):
+	f = open(fa, "r")
+	file = f.readlines()
+
+	sequences = []
+	seq = ""
+	for f in file:
+		if not f.startswith('>'):
+			f = f.replace(" ", "")
+			f = f.replace("\n", "")
+			seq = seq + f
+		else:
+			sequences.append(seq)
+			seq = ""
+
+	# Add the last seq
+	sequences.append(seq)
+
+	sequences = sequences[1:]
+
+	def get_all_repeats(sequence):
+		length = len(sequence)
+		repeats = []
+		for i in range(length):
+			repeats.append(sequence[i:i + (num*2)])
+		return repeats
+
+	all_six_repearts = []
+	for i in sequences:
+		repeats_list = get_all_repeats(i)
+		for j in repeats_list:
+			all_six_repearts.append(j)
+
+	def most_common(lst):
+		return max(set(lst), key=lst.count)
+
+	print(most_common(all_six_repearts))
+	print(all_six_repearts.count(most_common(all_six_repearts)))
+
 ###########################################
 	
 
 #Punto 6
 print 'Punto 6'
 punto6(fa)
+
+#Punto 7
+print 'gi|142022655|gb|EQ086233.1|97'
+num="gi|142022655|gb|EQ086233.1|97"
+find_idenfitier(num)
+
+#Punto 8,9 y 10
+num=6
+find_length(num)
 
 
 
